66ae6c758e05dd649d715b6cea7f1cbc9918a2ca
Pepe Valdivia <pepe.valdivia.caballero@gmail.com>
2022-02-03 21:44:36 -0500

    calculo de inventario final

--- app.rb
+++ app.rb
@@ -1,16 +1,13 @@
 prices= [5000, 6500, 7000, 6000, 8000, 9000, 6500]
-amount=[100, -2, -1 , 5, -0.5,  -0.5, -4.5]
+amount=[4, -2, -1 , 5, -0.5,  -0.5, -4.5]
 
 ii = 0
 index = 0
 ivalor = []
 
 #puts amount
-# puts amount.select{|x| x < 0}.sum
-# puts amount.select{|x| x > 0}.sum
 puts "venta, #{amount.select{|x| x < 0}.sum}"
 puts "compras, #{amount.select{|x| x > 0}.sum}"
-
 final = amount.select{|x| x > 0}.sum + amount.select{|x| x < 0}.sum
 #puts final
 ventas = 0
@@ -23,15 +20,32 @@ for i in 0..amount.length - 1
   if amount[i] > 0
     # ingreso material, costo
     costo = costo + amount[i] * prices[i]
+    inventario.push({
+      :costo => prices[i],
+      :cantidad => amount[i]
+    })
   else
     # venta
+    cantidad = amount[i]
+    for k in 0..inventario.length - 1
+      if (-1 * cantidad) > inventario[k][:cantidad]
+        cogs = cogs + inventario[k][:cantidad] * inventario[k][:costo]
+        if inventario[k][:cantidad] > 0
+          cantidad = cantidad + inventario[k][:cantidad]
+          inventario[k][:cantidad] = 0
+        end
+      else
+        cogs = cogs + cantidad * inventario[k][:costo]
+        inventario[k][:cantidad] = inventario[k][:cantidad] + cantidad
+        cantidad = 0
+      end
+    end
     ventas = ventas + -1 * (amount[i] * prices[i])
-    cogs
   end
 end
 
-x = ventas - costo
-puts x
+puts inventario
+puts cogs
 
 """
 amount.each do |e|
